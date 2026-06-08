502f4a8e68de2d413265054d6c584d455ab8e879
Mirza Ibrahim Baig <128467613+Ibrahimbaig2001@users.noreply.github.com>
2026-06-08 15:23:12 +0530

    Add files via upload
    You need it install express and mongoose along to get the node modules in your repo

---
+++ Backend-practice/server.js
@@ -0,0 +1,8 @@
+const app = require('./src/app')
+const mongoose = require('mongoose')
+const connectDB = require('./src/db/db')
+connectDB()
+
+app.listen(3000,() =>{
+    console.log("Server is running on port 3000")
+})
